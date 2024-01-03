d9869f052290e0e19e9650c27d4b446bbd038f84
Ravi Teja <94431973+RaviTeja-gunda@users.noreply.github.com>
2024-01-03 10:17:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -116,8 +116,8 @@ def predict_colleges(text, rank, gender, data, target_rank):
         st.write(f"Recommended colleges for rank {rank}, gender {gender}, and caste {student_caste}:")
         filtered_data=filtered_data.sort_values(by=target_rank)
         # st.table(filtered_data[table_columns])
-        # st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].head(30))
-        st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].to_string(index=False)
+        st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].head(30))
+        # st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].to_string(index=False)
         return
 
     # Recommend colleges
@@ -125,9 +125,9 @@ def predict_colleges(text, rank, gender, data, target_rank):
     st.subheader(
         f"Recommended colleges for rank {rank}, desired branch {desired_branch}, gender {gender}, and caste {student_caste}:")
     filtered_data=filtered_data.sort_values(by=target_rank)
-    st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].to_string(index=False)
+    # st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].to_string(index=False)
     # st.table(filtered_data[table_columns])
-    # st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].head(30))
+    st.table(filtered_data[["inst_code", target_rank, "COED", "branch_code", "FEE"]].head(30))
 
 # Streamlit UI
 st.title("EAPCET College List Recommendation System")
