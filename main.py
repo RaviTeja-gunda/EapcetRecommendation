b4e28b6d65df9e4ce28cae635889b33af589b8d6
Ravi Teja <94431973+RaviTeja-gunda@users.noreply.github.com>
2024-01-04 21:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ st.markdown(
     """
     <style>
     .stApp {
-        background-color: #5999A6; /* Change the color code to your desired background color */
+        background-color: #00FFD8; /* Change the color code to your desired background color */
     }
     </style>
     """,
